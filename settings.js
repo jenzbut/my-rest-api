f97a0fd1fffe7556604ba60f3a2681e48473cb12
jenzbut <145190041+jenzbut@users.noreply.github.com>
2023-09-27 07:20:21 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'JENZSTORE'// yourname
+global.MONGO_DB_URI = "mongodb+srv://jenzstore:jenzstore123@cluster0.2g8mg21.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
+global.your_email = "firazdengkol@gmail.com" //email
 global.email_password = "" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
